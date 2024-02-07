a7e3401473ddb662419729415d54b039f065f748
Ballard <ballard@button.is>
2024-02-07 03:07:38 -0500

    chore: add eslint workflow

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-# Check if ESLint is installed
-if ! command -v eslint &> /dev/null; then
-    echo "ESLint is not installed. Installing ESLint..."
-    yarn add eslint --dev
-fi
-
 pushd client || exit 1
 files=("$@")
 files=("${files[@]/#/../}") # add ../ to each element
